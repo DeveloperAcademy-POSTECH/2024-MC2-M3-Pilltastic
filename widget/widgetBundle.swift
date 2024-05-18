2ec34c7c380b5bfa3d90a675bfba94f4080d0538
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-18 18:02:28 +0900

    Feat: create widget extension

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Groo on 5/18/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetLiveActivity()
+    }
+}
